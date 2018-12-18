323de95a8e04aba7554194840983aafa7b58a30e
naristedyikanbaeva <dyikanbaevanariste@gmail.com>
2018-12-18 14:15:07 -0600

    second project

---
+++ src/day1/presentingGit.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class presentingGit {
+    public static void main(String[] args) {
+        System.out.println("Second push");
+    }
+    public static void newMethod(){
+        System.out.println("for git");
+    }
+}
